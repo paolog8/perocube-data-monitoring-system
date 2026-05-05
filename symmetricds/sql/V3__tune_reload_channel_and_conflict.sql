c84728e5ad75aef5694baae39d3b7def19e57490
paolog8 <paolograniero8@gmail.com>
2026-05-05 12:26:41 +0200

    Refactor sym_conflict insert statement to include target_channel_id and reorganize columns

--- symmetricds/sql/V3__tune_reload_channel_and_conflict.sql
+++ symmetricds/sql/V3__tune_reload_channel_and_conflict.sql
@@ -7,28 +7,34 @@ INSERT INTO sym_conflict (
     conflict_id,
     source_node_group_id,
     target_node_group_id,
+    target_channel_id,
     target_catalog_name,
     target_schema_name,
     target_table_name,
     detect_type,
+    detect_expression,
     resolve_type,
     ping_back,
-    use_row_data,
+    resolve_changes_only,
+    resolve_row_only,
     create_time,
-    last_update_time,
-    last_update_by
+    last_update_by,
+    last_update_time
 ) VALUES (
     'ignore_duplicates_on_reload',
     'edge',
     'hub',
+    NULL,          -- applies to all channels
     NULL,
     NULL,
     NULL,          -- applies to all tables
     'USE_PK_DATA',
+    NULL,
     'IGNORE',
     '0',
     '0',
+    '0',
     now(),
-    now(),
-    'admin'
+    'admin',
+    now()
 );
